8b34351e44c94302504364eae6037a9b1d25d952
Kris Logemann <krisrlogemann@gmail.com>
2019-07-31 14:44:52 -0700

    removes excess fat

--- README.md
+++
@@ -1,3 +0,0 @@
-Run npm install
-Run npm install -g loopback
-To Run app just run: node .
--- server/boot/root.js
+++ server/boot/root.js
@@ -1,7 +1,3 @@
-// Copyright IBM Corp. 2014,2016. All Rights Reserved.
-// Node module: loopback-example-passport
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
 
 'use strict';
 
